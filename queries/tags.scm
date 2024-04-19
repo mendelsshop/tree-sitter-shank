8258ff2751c2f62a2f1484c9075380caf62e43ef
mendelsshop <mendelsshop@gmail.com>
2024-04-19 14:12:30 -0400

    trying to add tags

---
+++ queries/tags.scm
@@ -0,0 +1,20 @@
+; record definition
+
+(record name: (identifier) @name) @definiton.class
+
+; record definition
+
+(enum name: (identifier) @name) @definiton.class
+
+
+; module definition
+
+(module name: (identifier) @name) @definiton.module
+
+; function definition
+
+(function_definition name: (identifier) @name) @defintion.function
+
+; calls
+
+(call_statement function  : (identifier) @name) @reference.call
